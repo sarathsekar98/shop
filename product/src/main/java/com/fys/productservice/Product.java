dc59f1810fd1ea73c47f059e9d27e268aa686c83
sarath <sarathsekar98@gmail.com>
2023-02-04 22:40:32 +0530

    first frontend and product commit here...

---
+++ product/src/main/java/com/fys/productservice/Product.java
@@ -0,0 +1,11 @@
+package com.fys.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Product {
+    public static void main(String[] args) {
+        SpringApplication.run(Product.class);
+    }
+}
